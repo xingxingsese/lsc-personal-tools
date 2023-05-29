73d497b27ec519ea07acdbcdaf1eed42588b1abb
lisc <zuihou098@qq.com>
2023-05-30 01:17:07 +0800

    项目初始化

---
+++ personal-tools-core/src/main/java/com/lsc/personaltools/core/PersonalToolsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.lsc.personaltools.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalToolsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalToolsCoreApplication.class, args);
+    }
+
+}
